7762e6c98ff1f125dcf82e58bf3c023c7c408dd2
Arun1only1 <arun1nly1@gmail.com>
2024-01-11 14:45:59 +0545

    read operation complete

---
+++ c.closer.look.to.read.operations.mongodb.js
@@ -0,0 +1,128 @@
+// closer look to read operations
+use("badminton");
+
+//? comparison operators
+
+// ?$eq
+// name ==="Under the Dome"
+// db.movies.find({ name: { $eq: "Under the Dome" } });
+
+// ? find movie whose name is Bitten
+// db.movies.find({ name: { $eq: "Bitten" } });
+
+// db.movies.find({ name: "Bitten" });
+
+// db.movies.find({ name: "Person of Interest" });
+
+// ? find movies whose rating is greater than or equal to 9
+// db.movies.find({ "rating.average": { $gte: 9 } }, { name: 1, rating: 1 });
+
+//? $gt
+//  runtime > 60
+// db.movies.find({ runtime: { $gt: 60 } }).count();
+// db.movies.find({ runtime: { $gt: 30 } });
+
+// ?gte
+// runtime>=60
+// db.movies.find({ runtime: { $gte: 60 } });
+
+// ? $lt
+// weight<75
+// db.movies.find({ weight: { $lt: 75 } });
+
+// ? $lte
+// weight <= 75
+// db.movies.find({ weight: { $lte: 75 } });
+
+// ?$not
+// status !=="Ended"
+// db.movies.find({ status: { $not: { $eq: "Ended" } } });
+
+// ? $ne => not equal
+// db.movies.find({ status: { $ne: "Ended" } });
+
+// ? $in
+// db.movies.find({
+//   "rating.average": { $in: [6, 6.5, 9, 8, 8.5, 8.6, 7.7, 3.1, 7.8] },
+// });
+
+// ? $nin
+// db.movies.find({
+//   "rating.average": { $nin: [6, 6.5, 9, 8, 8.5, 8.6, 7.7, 3.1, 7.8] },
+// });
+// =============================================================================
+// ? logical operator
+
+// ? $and, $or, $not, $nor
+
+// ?$and
+//  status==="Ended" && runtime ===60
+// db.movies.find({
+//   $and: [{ status: "Ended" }, { runtime: 60 }],
+// });
+
+// ?$or
+// find movie whose runtime is less than 60 or status is Ended
+// runtime<60 || status === "Ended"
+// db.movies.find(
+//   { $or: [{ runtime: { $lt: 60 } }, { status: "Ended" }] },
+//   {
+//     name: 1,
+//     status: 1,
+//     runtime: 1,
+//   }
+// );
+
+// ?$nor
+// find movie whose status is neither Ended nor runtime is 60
+
+// db.movies.find(
+//   { $nor: [{ status: "Ended" }, { runtime: 60 }] },
+//   {
+//     name: 1,
+//     status: 1,
+//     runtime: 1,
+//   }
+// );
+
+// ================================================================
+// ?array
+// ? $all, $elemMatch, $size
+
+// find movies whose genres is both Drama and Thriller
+// db.movies.find({ $and: [{ genres: "Drama" }, { genres: "Thriller" }] });
+
+// ? $all
+// db.movies.find({ genres: { $all: ["Thriller", "Drama"] } });
+
+// db.students.find({ hobbies: { $all: ["Dancing", "Singing"] } });
+
+// db.students.find({ "scores.sub": "Math", "scores.point": { $gt: 70 } });
+
+// db.students.find({
+//   scores: { $elemMatch: { sub: "Math", point: { $gt: 70 } } },
+// });
+
+// db.scores.find({
+//   results: { $gt: 80, $lt: 85 },
+// });
+
+// db.scores.find({ results: { $elemMatch: { $gt: 80, $lt: 85 } } });
+
+// ?$size
+// ? $size does not accept range
+// db.students.find({ hobbies: { $size: 3 } });
+
+// =============================================================
+// ? evaluation operator
+
+//? $regex
+// i => case insensitive
+// db.movies.find({ name: { $regex: "dome", $options: "i" } });
+
+// db.movies.find({ summary: { $regex: "400 inhabitants", $options: "i" } });
+
+// ?$expr
+// find sales data whose order is greater than volume
+// order>volume
+// db.sales.find({ $expr: { $gt: ["$order", "$volume"] } });
---
+++ d.assignment.mongodb.js
@@ -0,0 +1,120 @@
+use("badminton");
+
+// ?read operations
+
+//?Find movies whose status is "Ended"
+// db.movies.find({ status: "Ended" }, { name: 1, status: 1 });
+
+// ?find movies whose rating is 9
+// db.movies.find({ "rating.average": 9 });
+
+// ?find movies whose rating is greater than 7 and less than 9
+// db.movies.find({
+//   $and: [{ "rating.average": { $gt: 7 } }, { "rating.average": { $lt: 9 } }],
+// });
+
+// db.movies.find({ "rating.average": { $gt: 7, $lt: 9 } });
+
+// ?find movies whose genres is Thriller
+// db.movies.find({ genres: "Thriller" });
+
+// ?find movies whose status is Running
+// db.movies.find({ status: "Running" });
+
+//? find movies whose status is Ended and runtime is 60
+// db.movies.find({ $and: [{ status: "Ended" }, { runtime: 60 }] });
+
+// db.movies.find({ status: "Ended", runtime: 60 });
+// ?find movies whose weight is 75 and network country is Canada
+
+// db.movies.find({ weight: 75, "network.country.name": "Canada" });
+
+// ?find movies whose weight is 96 or runtime is 60
+// db.movies.find({ $or: [{ weight: 96 }, { runtime: 60 }] });
+
+// ? find movies whose rating average is not 9
+// db.movies.find({ "rating.average": { $ne: 9 } });
+
+// ? find movies whose rating average is either 6 or 6.5 or
+// ?9 or 8 or 8.5 or 8.6 or 7.7 or 6.1 or 7.8
+// db.movies.find({
+//   $or: [
+//     { "rating.average": 6 },
+//     { "rating.average": 6.5 },
+//     { "rating.average": 9 },
+//     { "rating.average": 8 },
+//     { "rating.average": 8.5 },
+//     { "rating.average": 8.6 },
+//     { "rating.average": 7.7 },
+//     { "rating.average": 6.1 },
+//     { "rating.average": 7.8 },
+//   ],
+// });
+
+// ? $in
+// db.movies.find({
+//   "rating.average": { $in: [6, 6.5, 9, 8, 8.5, 8.6, 7.7, 3.1, 7.8] },
+// });
+
+// ? find movies whose rating average is neither 6 nor 6.5 nor
+// ?9 nor 8 nor 8.5 nor 8.6 nor 7.7 nor 6.1 nor 7.8
+// db.movies.find({
+//   $nor: [
+//     { "rating.average": 6 },
+//     { "rating.average": 6.5 },
+//     { "rating.average": 9 },
+//     { "rating.average": 8 },
+//     { "rating.average": 8.5 },
+//     { "rating.average": 8.6 },
+//     { "rating.average": 7.7 },
+//     { "rating.average": 6.1 },
+//     { "rating.average": 7.8 },
+//   ],
+// });
+
+// ? $nin
+// db.movies.find({
+//   "rating.average": { $nin: [6, 6.5, 9, 8, 8.5, 8.6, 7.7, 3.1, 7.8] },
+// });
+
+// ?find movies whose genre includes Action and Crime
+//! db.movies.find({ genres: "Action", genres: "Crime" });
+
+// db.movies.find({ $and: [{ genres: "Action" }, { genres: "Crime" }] });
+
+// db.movies.find({ genres: { $all: ["Action", "Crime"] } });
+
+// ? find movies whose genres size is 2
+
+// db.movies.find({ genres: { $size:2 } });
+
+// ? find movies whose genres size is  not 2
+// db.movies.find({ genres: { $size: { $ne: 2 } } });
+
+// db.movies.find({ genres: { $not: { $size: 2 } } });
+
+// ? find movies whose language is not English
+// db.movies.find({ language: { $ne: "English" } });
+
+// ? find movies whose summary includes "Pacific"
+// db.movies.find({ summary: { $regex: "Pacific", $options: "i" } });
+
+// ? find students whose hobby is neither Swimming nor Dancing
+// db.students.find({ $nor: [{ hobbies: "Swimming" }, { hobbies: "Dancing" }] });
+
+// ?find students whose hobby is both Gaming and Singing
+// db.students.find({ hobbies: { $all: ["Gaming", "Singing"] } });
+
+// ?find students who have three hobbies
+// db.students.find({ hobbies: { $size: 3 } });
+
+// ?find students whose math score is greater than 50
+// db.students.find({
+//   scores: { $elemMatch: { sub: "Math", point: { $gt: 50 } } },
+// });
+
+// ?find students whose score in any subject is greater than 70
+// db.students.find({ "scores.point": { $gt: 70 } });
+
+// ? find students whose primary id is "656070d2b16daaa31169a51b"
+// db.students.find({ _id: ObjectId("656070d2b16daaa31169a51b") });
